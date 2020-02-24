877b06e66074a8c86ae617c800b8f47f329ffd78
IAmJamesHall <thatwhichendureth@gmail.com>
2020-02-24 10:01:10 -0500

    Added base components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+
+// Components
+import Search from './components/Search';
+import Nav from './components/Nav';
+import Results from './components/Results';
+import NoResults from './components/NoResults';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Search />
+      <Nav />
+      <Results />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Photo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Photo() {
+    return (
+        <li>
+            <img src="https://via.placeholder.com/150"></img>
+        </li>
+    );
+}
+
+export default Photo;
