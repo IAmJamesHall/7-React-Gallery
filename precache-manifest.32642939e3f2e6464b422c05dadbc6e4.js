a85c581ee96919720d236bfaf13555b6196a84ca
IAmJamesHall <thatwhichendureth@gmail.com>
2020-04-06 11:00:26 -0400

    Updates

---
+++ precache-manifest.32642939e3f2e6464b422c05dadbc6e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f22fe80342cde3012373430a1702894",
+    "url": "/Project7/index.html"
+  },
+  {
+    "revision": "91ea8fa65bbb40a19c17",
+    "url": "/Project7/static/css/main.8a163a0c.chunk.css"
+  },
+  {
+    "revision": "b59b5fd536d401c6ad59",
+    "url": "/Project7/static/js/2.124df04a.chunk.js"
+  },
+  {
+    "revision": "72bf0e41e952298354e3383942e2ce5d",
+    "url": "/Project7/static/js/2.124df04a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91ea8fa65bbb40a19c17",
+    "url": "/Project7/static/js/main.4dd201a9.chunk.js"
+  },
+  {
+    "revision": "dc1c151370d5bd8ebe8b",
+    "url": "/Project7/static/js/runtime-main.24167712.js"
+  }
+]);
